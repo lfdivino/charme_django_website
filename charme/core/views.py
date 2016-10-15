70502a3774eb093deff17154b4285ae3aa4b91b4
lf.divino <lf.divino@gmail.com>
2016-10-15 11:29:00 -0300

    Adicionado video destaque na pagina de videos

--- charme/core/views.py
+++ charme/core/views.py
@@ -112,7 +112,8 @@ def about(request):
 
 
 def video(request):
-    videos_ids = Videos.objects.all().order_by('-id')
+    videos_ids = Videos.objects.all().order_by('-id')[1:]
+    video_destaque = Videos.objects.all().order_by('-id')[0]
     paginator = Paginator(videos_ids, 12)
 
     page = request.GET.get('page')
@@ -125,6 +126,7 @@ def video(request):
 
     context = {
         'videos': videos,
+        'video_destaque': video_destaque,
     }
 
     return render(request, 'videos.html', context)
