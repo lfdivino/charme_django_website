f984109dbb80d2d50a95815606129013772565e6
lf.divino <lf.divino@gmail.com>
2016-10-26 12:02:53 -0200

    Mudancas na pagina de video

---
+++ charme/core/static/js/modal_video.js
@@ -0,0 +1,43 @@
+// BOOTSTRAP 3.0 - Open YouTube Video Dynamicaly in Modal Window
+// Modal Window for dynamically opening videos
+$('.video_youtube').on('click', function(e){
+  // Store the query string variables and values
+	// Uses "jQuery Query Parser" plugin, to allow for various URL formats (could have extra parameters)
+	var queryString = $(this).attr('href');
+
+    // Prevent opening of external page
+    e.preventDefault();
+
+    // Variables for iFrame code. Width and height from data attributes, else use default.
+    var vidWidth = 853; // default
+    var vidHeight = 480; // default
+    var iFrameCode = '<iframe width="' + vidWidth + '" height="'+ vidHeight +'" scrolling="no" allowtransparency="true" allowfullscreen="true" src="' + queryString +'?rel=0&wmode=transparent&showinfo=0" frameborder="0"></iframe>';
+
+    // Replace Modal HTML with iFrame Embed
+    $('#mediaModal .modal-body').html(iFrameCode);
+    // Set new width of modal window, based on dynamic video content
+    $('#mediaModal').on('show.bs.modal', function () {
+        // Add video width to left and right padding, to get new width of modal window
+        var modalBody = $(this).find('.modal-body');
+        var modalDialog = $(this).find('.modal-dialog');
+        var newModalWidth = vidWidth;
+        // newModalWidth += parseInt(modalDialog.css("padding-left")) + parseInt(modalDialog.css("padding-right"));
+        newModalWidth += 'px';
+        // Set width of modal (Bootstrap 3.0)
+        $(this).find('.modal-dialog').css('width', newModalWidth);
+        $(this).find('.modal-dialog').css('margin-top', '10%');
+        $(this).find('.modal-content').css('border-radius', 0);
+        $(this).find('.modal-content').css('height', vidHeight);
+        $(this).find('.modal-body').css('padding', 0);
+    });
+
+    // Open Modal
+    $('#mediaModal').modal();
+    $('.modal-backdrop').css('opacity', 0.95);
+});
+
+// Clear modal contents on close.
+// There was mention of videos that kept playing in the background.
+$('#mediaModal').on('hidden.bs.modal', function () {
+	$('#mediaModal .modal-body').html('');
+});
