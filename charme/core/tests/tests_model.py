3c732393d8297f519483c0e71735893ead7eaa9b
lf.divino <lf.divino@gmail.com>
2016-10-30 23:06:01 -0200

    Correcao na pagina de teste

--- charme/core/tests/tests_model.py
+++ charme/core/tests/tests_model.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from charme.core.models import Post, Author
+from charme.core.models import PostsBlog, Author, PostCategories
 
 
 class BlogTest(TestCase):
@@ -8,10 +8,13 @@ class BlogTest(TestCase):
     def setUp(self):
         self.author = Author(name="Luiz Felipe", email="lf.divino@gmail.com")
         self.author.save()
-        self.post = Post(
+        self.category = PostCategories(name="Moda")
+        self.category.save()
+        self.post = PostsBlog(
             title="Primeiro Post no site",
             body="Este é o primeiro post que eu faço no site Charme Todo Dia!",
-            author=self.author
+            author=self.author,
+            category=self.category
         )
         self.post.save()
 
--- charme/core/tests/tests_template.py
+++ charme/core/tests/tests_template.py
@@ -1,16 +1,1 @@
 from django.test import TestCase
-
-
-class BlogTest(TestCase):
-
-    def setUp(self):
-        self.author = Author(name="Luiz Felipe", email="lf.divino@gmail.com")
-        self.author.save()
-        self.post = Post(
-            title="Primeiro Post no site",
-            body="Este é o primeiro post que eu faço no site Charme Todo Dia!",
-            author=self.author
-        )
-        self.post.save()
-
-   # def test_post_index(self):
